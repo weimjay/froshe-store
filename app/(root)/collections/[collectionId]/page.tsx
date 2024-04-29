4245e27da9dcdd724a2e3f6afd0d719aa544ed76
weimjay <weimjay@gmail.com>
2024-04-29 20:56:16 +1000

    fix

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -2,6 +2,7 @@ import ProductCard from "@/components/ProductCard";
 import { getCollectionDetails } from "@/lib/actions/actions";
 import Image from "next/image";
 import React from "react";
+import useWishlist from "@/lib/hooks/useWishlist";
 
 const CollectionDetails = async ({
   params,
@@ -9,6 +10,7 @@ const CollectionDetails = async ({
   params: { collectionId: string };
 }) => {
   const collectionDetails = await getCollectionDetails(params.collectionId);
+  const wishlist = await useWishlist();
 
   return (
     <div className="px-10 py-5 flex flex-col items-center gap-8">
@@ -23,7 +25,7 @@ const CollectionDetails = async ({
       <p className="text-body-normal text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
       <div className="flex flex-wrap gap-16 justify-center">
         {collectionDetails.products.map((product: ProductType) => (
-          <ProductCard key={product._id} product={product} />
+          <ProductCard key={product._id} product={product} liked={wishlist.includes(product._id)} />
         ))}
       </div>
     </div>
