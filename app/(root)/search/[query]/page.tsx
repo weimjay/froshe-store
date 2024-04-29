23019e794390960e5dafbf88cdf5ee8a10c14b8b
weimjay <weimjay@gmail.com>
2024-04-29 20:59:07 +1000

    fix

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -1,10 +1,12 @@
 import ProductCard from '@/components/ProductCard'
 import { getSearchedProducts } from '@/lib/actions/actions'
+import useWishlist from "@/lib/hooks/useWishlist";
 
 const SearchPage = async ({ params }: { params: { query: string }}) => {
   const searchedProducts = await getSearchedProducts(params.query)
 
   const decodedQuery = decodeURIComponent(params.query)
+  const wishlist = await useWishlist();
 
   return (
     <div className='px-10 py-5'>
@@ -14,7 +16,7 @@ const SearchPage = async ({ params }: { params: { query: string }}) => {
       )}
       <div className='flex flex-wrap justify-between gap-16'>
         {searchedProducts?.map((product: ProductType) => (
-          <ProductCard key={product._id} product={product} />
+          <ProductCard key={product._id} product={product} liked={wishlist.includes(product._id)} />
         ))}
       </div>
     </div>
